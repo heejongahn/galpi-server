6191006f365336625b3725c7ed87fa88c7602e6d
Heejong Ahn <heejongahn@gmail.com>
2020-05-30 15:36:44 +0900

    fixup! Allow cors for review/get

--- src/handlers/review/get.ts
+++ src/handlers/review/get.ts
@@ -44,6 +44,10 @@ const index: Handler<APIGatewayEvent> = async (event) => {
 
     return {
         statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+        },
         body: JSON.stringify({ review }),
     };
 };
