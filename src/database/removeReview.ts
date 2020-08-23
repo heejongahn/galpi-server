1211d50d305b8210bdd934bad4094e48b5fe55e8
Heejong Ahn <heejongahn@gmail.com>
2020-08-23 23:14:38 +0900

    Fix removeReview

--- src/database/removeReview.ts
+++ src/database/removeReview.ts
@@ -1,5 +1,6 @@
 import { Review } from '../entity/Review';
 import { getConnection } from '.';
+import { Revision } from '../entity/Revision';
 
 interface Props {
     review: Review;
@@ -10,10 +11,17 @@ export async function removeReview({ review }: Props) {
 
     await connection.transaction(async (manager) => {
         const reviewRepository = manager.getRepository(Review);
+        const revisionRepository = manager.getRepository(Revision);
 
         await reviewRepository.update(review.id, {
             activeRevision: undefined,
         });
+
+        const { revisions } = (await reviewRepository.findOne({ id: review.id }, { relations: ['revisions'] })) || {};
+
+        if (revisions) {
+            await revisionRepository.remove(revisions);
+        }
 
         await reviewRepository.remove([review]);
     });
